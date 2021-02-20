f47bd9592de8b5ee464278c015df77ae996a7444
Daniil <37474366+DaniilAfendulov@users.noreply.github.com>
2021-02-20 18:38:41 +0300

    Update compress.scala
    Imported tailrec

--- compress.scala
+++ compress.scala
@@ -1,4 +1,6 @@
+import scala.annotation.tailrec
 def compress(list:List[Any]): List[Any] = compressr(list, List(), Nil)
+@tailrec
 def compressr(list:List[Any], listr:List[Any], last:Any): List[Any] = {
     list match {
       case head :: tail if head==last  => compressr(tail, listr, head)
